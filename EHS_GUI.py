65c93eac4f339219675cf7c8f88bccb43a03ab4f
Ellen Guo <elleng18@vt.edu>
2022-02-24 17:07:44 -0500

    obtains the <tr> and <td> table tags

--- EHS_GUI.py
+++ EHS_GUI.py
@@ -4,19 +4,26 @@ from bs4 import BeautifulSoup
 
 EHS_url = 'https://www.ehss.vt.edu/programs/ROOF_access_chart_050916.php'
 
-
 def main():
     r = requests.get(url=EHS_url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    last_update_date = soup.find("div", {"class": "content-block"}).find("caption").getText().\
-        replace('\r\n                    ', '') # obtain caption element; remove extra space in the caption element tag
+    last_update_date = soup.find("div", {"class": "content-block"}).find("caption").getText(). \
+        replace('\r\n                    ', '')  # obtain caption element; remove extra space in the caption element tag
     print(last_update_date)
-    # updated = soup.find_all("caption")
-    # print(updated.getText())
-    print("hello")
+    content = soup.find("div", {"class": "content-block"}).findAll("tr")  # obtain rows of the hazard table
+    r = len(content)
+    columns = content[0].findAll("td")
+    c = len(columns)
+    table_content = np.zeros((r, c))
+    for i in range(r):
+        for j in range(c):
+            labels = content[i].findAll("td")[j].getText().replace('\n', ' ')
+            table_content[i, j] = labels
+            print("hello")
+    # print(labels)
+    # print("hello")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
