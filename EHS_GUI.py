985e8b1d2203373e94937acb234dfdc47ccdb31a
Ellen Guo <elleng18@vt.edu>
2022-03-15 10:30:02 -0400

    Qt Checkbox and ScrollBar added for Building Selection

--- EHS_GUI.py
+++ EHS_GUI.py
@@ -1,10 +1,47 @@
 import requests
 import numpy as np
+import sys
 from bs4 import BeautifulSoup
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QScrollArea, QFormLayout, QGroupBox, \
+    QVBoxLayout, QLabel, QCheckBox, QButtonGroup
 
 EHS_url = 'https://www.ehss.vt.edu/programs/ROOF_access_chart_050916.php'
 
-def main():
+
+class EHSWindow(QMainWindow):  # EHS GUI Class
+
+    def __init__(self):
+        super().__init__()
+        self.title = "EHS Safety Application"
+        self.setWindowTitle(self.title)
+
+        widget = QWidget()
+
+        form_layout = QFormLayout()
+        group_box = QGroupBox()
+
+        label_list = []
+        buildings = scrape()
+
+        for i in range(len(buildings)):
+            label_list.append(QCheckBox(buildings[i]))
+            form_layout.addRow(label_list[i])
+
+        group_box.setLayout(form_layout)
+        scroll = QScrollArea()
+        scroll.setWidget(group_box)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(400)
+
+        layout = QVBoxLayout()
+        layout.addWidget(scroll)
+
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+
+def scrape():
     r = requests.get(url=EHS_url)
     soup = BeautifulSoup(r.content, 'html.parser')
     last_update_date = soup.find("div", {"class": "content-block"}).find("caption").getText(). \
@@ -13,15 +50,25 @@ def main():
     content = soup.find("div", {"class": "content-block"}).findAll("tr")  # obtain rows of the hazard table
     r = len(content)
     columns = content[0].findAll("td")
-    c = len(columns)
-    table_content = np.zeros((r, c))
-    for i in range(r):
-        for j in range(c):
-            labels = content[i].findAll("td")[j].getText().replace('\n', ' ')
-            table_content[i, j] = labels
-            print("hello")
+    building_name = []
+    for i in range(r-1):
+        building_name.append(content[i+1].find("td").getText())  # Scrap all the building names into a list
+    print("hello")
+    # for i in range(r):
+    #     for j in range(c):
+    #         labels = content[i].findAll("td")[j].getText().replace('\n', ' ')
+    #         table_content[i, j] = labels
+    #         print("hello")
     # print(labels)
     # print("hello")
+    return building_name
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = EHSWindow()  # window object
+    win.showFullScreen()  # Display GUI Full screen
+    sys.exit(app.exec_())  # exit
 
 
 # Press the green button in the gutter to run the script.
