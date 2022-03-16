4de9eb898e91a9a4814dc0774f82c6390aadc07f
Ellen Guo <elleng18@vt.edu>
2022-03-16 14:54:18 -0400

    Display table scarp in table

--- EHS_GUI.py
+++ EHS_GUI.py
@@ -1,7 +1,8 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QLabel, QComboBox, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QLabel, \
+    QComboBox, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
 
 EHS_url = 'https://www.ehss.vt.edu/programs/ROOF_access_chart_050916.php'
 
@@ -52,6 +53,8 @@ class EHSWindow(QMainWindow):  # EHS GUI Class
 
     def on_click(self):
         self.clear()
+        widget = QWidget()
+        vbox = QVBoxLayout()
         soup = query()
         content = soup.find("div", {"class": "content-block"}).findAll("tr")
         building_list = build_list()
@@ -62,8 +65,35 @@ class EHSWindow(QMainWindow):  # EHS GUI Class
         c = len(content[0].findAll("td"))
         for j in range(c-1):  # scrap hazard information from table on EHS website
             hazards.append(content[ind + 1].findAll("td")[j+1].getText().replace('\n', ' '))
-        print(types)
-        print(hazards)
+        self.building_name = QLabel(building + " EHS Hazard Information: ")  # QLabel Widget
+        self.table = QTableWidget()  # QTableWidget
+        # Row & Column Count for Qt Table
+        self.table.setRowCount(2)
+        self.table.setColumnCount(5)
+        # Place parsed information into table
+        for i in range(c-1):
+            self.table.setItem(0, i, QTableWidgetItem(types[i]))
+            self.table.setItem(1, i, QTableWidgetItem(hazards[i]))
+        # delete header numbers & Fit Screen Horizontally
+        self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().setVisible(False)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        # ********* GUI Info Page Layout **********
+        # _________________________________
+        # |         |           |          |
+        # |_________|___________|__________|
+        # |          [Building]            |
+        # |_________[Information]__________|
+        # |         |           |          |
+        # |         |           |          |
+        # |_________|___________|__________|
+        vbox.addStretch(2)
+        vbox.addWidget(self.building_name)
+        vbox.addWidget(self.table)
+        vbox.addStretch(2)
+        widget.setLayout(vbox)
+        self.setCentralWidget(widget)
 
     # clear widgets from layout
     def clear(self):
